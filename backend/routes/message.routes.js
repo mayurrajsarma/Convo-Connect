207fc0a48da00fa8aa4fa9e501b5328e840aad88
mayurrajsarma <mayurrajsarma202@gmail.com>
2024-05-15 12:19:24 +0530

    msg routes

---
+++ backend/routes/message.routes.js
@@ -0,0 +1,11 @@
+import express from "express" ;
+import {sendMessage} from "../controllers/message.controller.js";
+import protectRoute from "../middleware/protectRoute.js";
+const router = express.Router() ;
+
+//before we run 'sendMessage' we check if the user is logged in => protectRoute
+router.get("/:id",protectRoute,getMessage);
+
+router.post("/send/:idinlink",protectRoute,sendMessage);
+
+export default router ;
