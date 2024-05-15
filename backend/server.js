10be4c448bf5eb434db6d16b6aac15499c6bbfa4
mayurrajsarma <mayurrajsarma202@gmail.com>
2024-05-15 12:20:51 +0530

    server commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express" ;
 import dotenv from "dotenv" ;
 import authRoutes from "./routes/auth.routes.js" ;
+import messageRoutes from "./routes/message.routes.js " ;
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
 
 const app = express() ;
 const PORT = process.env.PORT || 8080
@@ -24,6 +26,8 @@ dotenv.config() ;
 
 app.use(express.json()) ;// to parse the incoming requests from req.body with JSON payload
 app.use("/api/auth", authRoutes) ; // whenever we visit anything starting with api/auth we will go to authRoutes
+app.use(cookieParser()) ;
+app.use("/api/messages", messageRoutes)
 
 // app.get("/",(req,res)=> {
 //     //root route http://localhost:8080/
