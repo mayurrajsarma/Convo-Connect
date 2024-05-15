31c0889954d45c043eea508b2632b0a157ba9970
mayurrajsarma <mayurrajsarma202@gmail.com>
2024-05-15 12:18:22 +0530

    msg and conv model

---
+++ backend/models/message.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderId: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    receiverId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    message:{
+        type:String,
+        required: true
+    }
+},{timestamps:true}) ;//later we can retreive the createdAt and updatedAt timestamp
+
+const Msg = mongoose.model("Message",messageSchema) ;
+export default Msg ;
