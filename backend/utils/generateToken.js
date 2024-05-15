7b473b5c55b39fb2fb22d7b29c14f7430f51bfde
mayurrajsarma <mayurrajsarma202@gmail.com>
2024-05-15 12:15:18 +0530

    generate  jwt token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,8 +4,8 @@ const generateTokenAndSetCookie = (userId,res) => {
     const token = jwt.sign({userId},process.env.JWT_SECRET, {
         expiresIn:"15d",
     });
-
-    res.cookie("jwt",token,{
+    //sending token to user cookie
+    res.cookie("jwt",token,{ //jwt is the cookie name.
         maxAge:15 * 24 * 60 * 60 * 1000, // in mili-second
         httpOnly:true,//prevent XSS attacks cross-site scripting attacks
         sameSite:"strict",//CSRF attacks cross site request forgery attacks
