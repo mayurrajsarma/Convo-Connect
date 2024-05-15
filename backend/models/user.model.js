bec000a4a10dd936be5b02df676039485e9eb129
mayurrajsarma <mayurrajsarma202@gmail.com>
2024-05-15 12:17:10 +0530

    user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
         type:String,
         default: "",
     },
-});
+},{timestamps:true});
 
-const User = mongoose.model("User",userSchema) ;
+const User= mongoose.model("User",userSchema) ;
 export default User ;
